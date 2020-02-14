2607029502b460008fb098c641420ef9e6def59b
Eileen Santiago <eileenmvs@gmail.com>
2020-02-14 15:55:47 -0500

    initial npm init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import Button from './ui/Button/Button.js';
+import Header from './ui/Header/Header.js';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <Header homeText="Bonjour"/>
+
+      Form here:
+      <Button text="Submit" styleType="primary"/>
+      <Button text="Cancel" styleType="secondary"/>
+      <Button text="Cancel" styleType="primary"/>
+      <Button text="Cancel" styleType="tertiary"/>
+    </div>
+  );
+}
+
+export default App;
