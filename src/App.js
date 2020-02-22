cfdf2d02230c591b2e85fccb1f226eaf4491085d
Eileen Santiago <eileenmvs@gmail.com>
2020-02-22 11:38:34 -0500

    added sass and bootstrap alongside initialized styles folder with respective stylesheets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Button from './ui/Button/Button.js';
 import Header from './ui/Header/Header.js';
+import './styles/main.scss';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       
-      <Header homeText="Bonjour"/>
+      <Header homeText="PaperSpace"/>
 
       Form here:
       <Button text="Submit" styleType="primary"/>
