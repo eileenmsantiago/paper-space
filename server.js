ccbf72b819e385e329e91db1b82996490bf826db
iamobyy <54857773+iamobyy@users.noreply.github.com>
2020-04-05 15:45:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express(); 
 
 // middleware  
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
